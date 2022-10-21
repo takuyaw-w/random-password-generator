056265485e825e088ea123efe1585dc87553a3cb
takuyaw-w <takuya.jamtb@gmail.com>
2022-10-22 00:42:41 +0900

    update

--- components/Table.tsx
+++ components/Table.tsx
@@ -10,24 +10,42 @@ type Header = {
 
 export function Table(props: Props) {
   const { headers, rows } = props;
+  const hasData = rows.length > 0;
+  const numOfColumn = headers.length;
   return (
-    <table class="min-w-full table-auto">
-      <thead class="bg-gray-800">
-        <tr class="text-white">
-          {headers.map((h) => <th class="p-4" key={h}>{h.headerName}</th>)}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((r, i) => {
-          return (
-            <tr class={i % 2 === 0 ? "bg-gray-50" : "bg-gray-200"} key={r}>
-              {headers.map((h) => {
-                return <td class="p-4 text-center" key={h}>{r[h.field]}</td>;
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div class="overflow-hidden rounded">
+      <table class="min-w-full">
+        <thead class="bg-gray-800">
+          <tr>
+            {headers.map((h) => (
+              <th class="text-white p-4" key={h}>{h.headerName}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {hasData
+            ? rows.map((r, i) => {
+              return (
+                <tr class={i % 2 === 0 ? "bg-gray-50" : "bg-gray-200"} key={r}>
+                  {headers.map((h) => {
+                    return (
+                      <td class="p-4 text-center" key={h}>
+                        {r[h.field]}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })
+            : (
+              <tr class="bg-gray-50">
+                <td colSpan={numOfColumn} class="p-4 text-center">
+                  Empty Data...
+                </td>
+              </tr>
+            )}
+        </tbody>
+      </table>
+    </div>
   );
 }
