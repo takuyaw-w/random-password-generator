3311b5890baf51a8947e174a09945611a6a7b231
takuyaw-w <takuya.jamtb@gmail.com>
2022-10-23 15:49:21 +0900

    fix: iroiro

--- utilis/bcrypto.ts
+++ utilis/bcrypto.ts
@@ -1,11 +1,19 @@
 import { hash as hashPromise, hashSync } from "bcrypt";
 import { cryptoRandomString } from "crypto_random_string";
 import { delay } from "std/async/delay.ts";
+const encoder = new TextEncoder();
 type GenerateOptions = {
   length: number;
   type?: CharacterType;
   characters?: string;
 };
+
+export type DigestType =
+  | "bcrypto"
+  | "SHA-1"
+  | "SHA-256"
+  | "SHA-384"
+  | "SHA-512";
 
 type CharacterType =
   | "hex"
@@ -28,15 +36,30 @@ const hash: typeof hashPromise = (
 
 export async function* generatePassword(
   quantity: number,
+  digestType: DigestType,
   options: GenerateOptions,
 ): AsyncGenerator<PasswordList> {
   for (let i = 0; i < quantity; i++) {
     const s = cryptoRandomString(options);
-    const h = await hash(s);
+    const h = await digest(s, digestType);
     await delay(0);
     yield {
       password: s,
       hash: h,
     };
+  }
+}
+
+async function digest(message: string, digestType: DigestType) {
+  if (digestType === "bcrypto") {
+    return hash(message);
+  } else {
+    const msgUint8 = encoder.encode(message);
+    const hashBuffer = await crypto.subtle.digest(digestType, msgUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join(
+      "",
+    );
+    return hashHex;
   }
 }
